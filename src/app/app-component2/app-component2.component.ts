5784f6b04036b6281d4613a487234be40dbd7412
ElenaAgeeva2505 <agatamisteri2505@mail.ru>
2022-11-23 22:17:09 +0300

    second task

---
+++ src/app/app-component2/app-component2.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-component2',
+  templateUrl: './app-component2.component.html',
+  styleUrls: ['./app-component2.component.css']
+})
+export class AppComponent2Component implements OnInit {
+
+ // constructor() { }
+
+  ngOnInit() {}
+
+  counter = 0;
+
+  changeCount(type: boolean) {
+    type ? this.counter++ : this.counter--;
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppComponent2Component } from './app-component2/app-component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponent2Component
   ],
   imports: [
     BrowserModule
